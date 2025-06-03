e3c938ca47b0045e049f5e140423348df626f236
shiva-kudumula <kudumulashiva510@gmail.com>
2025-06-03 12:34:58 +0530

    done

--- index.js
+++ index.js
@@ -1,50 +1,65 @@
 import express from 'express'
-import mongoose from 'mongoose'
 import cors from 'cors'
+import mongoose from 'mongoose'
+const app = express();
+app.listen(8080,()=>{
+  mongoose.connect("mongodb://localhost:27017/gcet")
+  console.log("Server Started");
+});
+const userSchema =mongoose.Schema({
+  name: {type: String},
+  email: {type: String},
+  pass: {type: String}
+});
+const User = mongoose.model("User", userSchema);
 
-const app = express()
 
-mongoose.connect("mongodb://localhost:27017/gcet")
-  .then(() => {
-    app.listen(8080, () => {
-      console.log("Server Started")
-    })
-  })
+app.use(cors());
+app.use(express.json())
+app.get("/", (req, res)=>{
+  return res.send("Good Morning!!");
+});
 
-const userSchema = new mongoose.Schema({
-  name: { type: String }
+app.post("/register",async(req,res)=>{
+  const {name, email, pass} = req.body;
+     const result = await User.create({ name: name, email: email, pass: pass });
+    res.json(result);
 })
 
-const User = mongoose.model("User", userSchema)
 
-app.use(cors())
+app.post("/login", async (req, res) => {
+  const { email, pass } = req.body;
 
-app.get("/", (req, res) => {
-  res.send("Good Morning!!")
+  const user = await User.findOne({ email, pass });
+
+  if (user) {
+    res.send({ message: "Login successful" });
+  } else {
+    res.send({  message: "Invalid email or password" });
+  }
+});
+app.get("/greet", (req, res)=>{
+  res.send("Greetings!!");
+} );
+
+app.get("/name", (req, res)=>{
+  res.send("Peter");
+} );
+
+app.get("/weather", (req, res)=>{
+  res.send("29 degrees");
+});
+
+app.get("/products", (req, res)=>{
+  const products=[
+    {name: "Product1", price:45},
+    {name: "Product2", price:50},
+    {name: "Product3", price:60},
+  ];
+  res.json(products);
 })
 
-app.get("/greet", (req, res) => {
-  res.send("Greetings!!")
-})
-
-app.get("/name", (req, res) => {
-  res.send("Shiva")
-})
-
-app.get("/register", async (req, res) => {
-  const result = await User.create({ name: "john" })
-  res.json(result)
-})
-
-app.get("/weather", (req, res) => {
-  res.send("29 degrees")
-})
-
-app.get("/products", (req, res) => {
-  const products = [
-    { name: "Product1", price: 45 },
-    { name: "Product2", price: 50 },
-    { name: "Product3", price: 60 }
-  ]
-  res.json(products)
-})
+app.get("/product", async (req, res) => {
+  const products = await products.find();
+  res.json(products);
+});
