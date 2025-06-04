7e7a9f49a6da432c2b296a282297f132a90c4e4b
shiva-kudumula <kudumulashiva510@gmail.com>
2025-06-04 15:31:31 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,35 +1,17 @@
 import express from "express";
-import userModel from "../models/userModel.js";
+import orderModel from "../models/orderModel.js";
 
-const userRouter = express.Router();
+const orderRouter = express.Router();
 
-userRouter.post("/register", async (req, res) => {
-  const { name, email, pass } = req.body;
-  const result = await userModel.insertOne({
-    name: name,
-    email: email,
-    pass: pass,
-  });
-  return res.json(result);
+orderRouter.post("/new", async (req, res) => {
+  try {
+    const { email, orderValue } = req.body;
+    const result = await orderModel.create({ email, orderValue });
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error("Error creating order:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-userRouter.post("/login", async (req, res) => {
-  const { email, pass } = req.body;
-  const result = await userModel.findOne({ email, pass });
-  if (!result) return res.json({ message: "Invalid user or password" });
-  return res.json(result);
-});
-
-userRouter.get("/:id", async (req, res) => {
-  const email = req.params.id;
-  const result = await userModel.findOne({ email });
-  return res.json(result);
-});
-
-userRouter.get("/:id/name", async (req, res) => {
-  const email = req.params.id;
-  const result = await userModel.findOne({ email },{_id:0,name:1});
-  return res.json(result);
-});
-
-export default userRouter;
+export default orderRouter;
